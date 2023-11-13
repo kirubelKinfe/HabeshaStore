c6087278559c4e4b2800314dae2f25e0cdead293
kirubeKinfe <kirubelkinfe01@gmail.com>
2023-11-13 22:50:15 +0300

    creating necessary components

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Product from "./Product";
+import Products from "./Products";
+import Search from "./Search";
+import Banner from "./Banner";
+import Cart from "./Cart";
+import Categories from "./Categories";
+import Checkout from "./Checkout";
+import Featured from "./Featured";
+
+export {
+    Navbar,
+    Product,
+    Products,
+    Search,
+    Banner,
+    Cart,
+    Categories,
+    Checkout,
+    Featured
+}
